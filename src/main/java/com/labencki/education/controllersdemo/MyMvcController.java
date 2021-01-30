fdadcd269facbfd245a131df62059fe4f3a0c24c
MichalLabencki <74935701+MichalLabencki@users.noreply.github.com>
2021-01-30 13:38:06 +0100

    Sobotni Kurs

--- src/main/java/com/labencki/education/controllersdemo/MyMvcController.java
+++ src/main/java/com/labencki/education/controllersdemo/MyMvcController.java
@@ -1,19 +1,29 @@
 package com.labencki.education.controllersdemo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-@RequestMapping("MyMvcController")
+@RequestMapping("users")
 public class MyMvcController {
 
     @GetMapping
-    String getNameViaMvc(@RequestParam(name = "imie") String name) {
+    String handleUsers(@RequestParam("user") String user, Model model) {
+        System.out.println("Handle Users");
+        List<String> nowaLista = new ArrayList<>();
+        for(int i=0;i<5;i++) {
+            nowaLista.add(user);
+
+        }
+        model.addAttribute("nowaLista",nowaLista);
         return "hello";
+
     }
+
 
 
 }
--- src/main/java/com/labencki/education/controllersdemo/MyRestController.java
+++ src/main/java/com/labencki/education/controllersdemo/MyRestController.java
@@ -3,16 +3,34 @@ package com.labencki.education.controllersdemo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 @RestController
 @RequestMapping("MyRestController")
 public class MyRestController {
 
     @GetMapping("{name}")
-    String getNameViaRest(@PathVariable(name="name") String name) {
+    String getNameViaRest(@PathVariable(name = "name") String name) {
         return "Hello " + name;
+
+
     }
 
+    @PostMapping
+    List<String> addUsersToModal(@RequestBody List<String> users) {
+        System.out.println("adding users to modal".toUpperCase());
+        List<String> nowaLista = new ArrayList<>();
+        for (String user : users) {
+            nowaLista.add(user.toUpperCase());
+        }
 
+        return nowaLista;
+
+
+//        return users;
+    }
 
 
 }
